9949e833f021be25f889b0759a5d905e9d837829
Olivia <olivia_git@outlook.com>
2016-12-26 09:34:05 +0200

    Adding members.

--- src/ImageSharp/Colors/Colorspaces/CieLuv.cs
+++ src/ImageSharp/Colors/Colorspaces/CieLuv.cs
@@ -6,12 +6,45 @@
 namespace ImageSharp.Colors.Colorspaces
 {
     using System;
+    using System.Numerics;
 
     /// <summary>
     /// CieLuv - https://en.wikipedia.org/wiki/CIELUV
     /// </summary>
     public struct CieLuv : IEquatable<CieLuv>, IAlmostEquatable<CieLuv, float>
     {
+        /// <summary>
+        /// The backing vector for SIMD support.
+        /// </summary>
+        private Vector3 backingVector;
+
+        /// <summary>
+        /// Lightness component
+        /// <remarks>
+        /// A value ranging between 0 and 100.
+        /// http://cs.haifa.ac.il/hagit/courses/ist/Lectures/Demos/ColorApplet/me/infoluv.html
+        /// </remarks>
+        /// </summary>
+        public float L => this.backingVector.X;
+
+        /// <summary>
+        /// U component
+        /// <remarks>
+        /// A value ranging between -134 and 220.
+        /// http://cs.haifa.ac.il/hagit/courses/ist/Lectures/Demos/ColorApplet/me/infoluv.html
+        /// </remarks>
+        /// </summary>
+        public float U => this.backingVector.Y;
+
+        /// <summary>
+        /// V component
+        /// <remarks>
+        /// A value ranging between -140 and 122.
+        /// http://cs.haifa.ac.il/hagit/courses/ist/Lectures/Demos/ColorApplet/me/infoluv.html
+        /// </remarks>
+        /// </summary>
+        public float V => this.backingVector.Z;
+
         /// <inheritdoc/>
         public bool AlmostEquals(CieLuv other, float precision)
         {
