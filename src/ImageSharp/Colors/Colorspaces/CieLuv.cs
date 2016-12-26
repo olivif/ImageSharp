18c26f77e8cc6dac28e8ebba71eb8b0cde0c195c
Olivia <olivia_git@outlook.com>
2016-12-26 09:22:42 +0200

    Adding CieLuv struct.

---
+++ src/ImageSharp/Colors/Colorspaces/CieLuv.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="CieLuv.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Colors.Colorspaces
+{
+    using System;
+
+    /// <summary>
+    /// CieLuv - https://en.wikipedia.org/wiki/CIELUV
+    /// </summary>
+    public struct CieLuv : IEquatable<CieLuv>, IAlmostEquatable<CieLuv, float>
+    {
+        /// <inheritdoc/>
+        public bool AlmostEquals(CieLuv other, float precision)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(CieLuv other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
